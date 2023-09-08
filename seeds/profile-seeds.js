828ce3ea48b7efcabdc90c9d6d72bcf59fbca6f9
Felipe <felipe.sarmiento.as@gmail.com>
2023-09-08 00:55:18 -0400

    homepage and meetup page dynamically rendering

--- seeds/profile-seeds.js
+++ seeds/profile-seeds.js
@@ -17,6 +17,38 @@ const profileData = [
     user_id: 2,
     location_id: 2,
   },
+  {
+    name: "Profile 3",
+    description: "Description for Profile 3",
+    phone: "9876543210",
+    image: "base64_encoded_image_data_for_profile_2",
+    user_id: 3,
+    location_id: 2,
+  },
+  {
+    name: "Profile 4",
+    description: "Description for Profile 4",
+    phone: "9876543210",
+    image: "base64_encoded_image_data_for_profile_2",
+    user_id: 4,
+    location_id: 5,
+  },
+  {
+    name: "Profile 5",
+    description: "Description for Profile 5",
+    phone: "9876543210",
+    image: "base64_encoded_image_data_for_profile_2",
+    user_id: 5,
+    location_id: 9,
+  },
+  {
+    name: "Profile 6",
+    description: "Description for Profile 6",
+    phone: "9876543210",
+    image: "base64_encoded_image_data_for_profile_2",
+    user_id: 6,
+    location_id: 8,
+  },
 ];
 
 const seedProfiles = () => Profile.bulkCreate(profileData);
